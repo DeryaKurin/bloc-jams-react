4fb30dbc301e358874b58236a0f18feb4ec3aa71
Derya O. Kurin <dozdemir663@gmail.com>
2018-12-16 12:59:01 -0500

    Checkpoint 9 interfaces: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,11 +11,32 @@ class Album extends Component {
     this.state ={
       album: album,
       currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration,
       isPlaying: false,
       isHovered: null
     };
     this.audioElement = document.createElement('audio');
     this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  componentDidMount() {
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      }
+    };
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount() {
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
   }
 
   play() {
@@ -70,8 +91,6 @@ class Album extends Component {
     } else {
       if (this.state.isPlaying && isSameSong) {
         btn = <span className="icon ion-md-pause"></span>
-      } else if (!this.state.isPlaying && isSameSong) {
-        btn = <span className="icon ion-md-play"></span>
       } else {
         btn = index + 1;
       }
@@ -93,6 +112,12 @@ class Album extends Component {
     let newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play();
+  }
+
+  handleTimeChange(e) {
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
   }
 
   render() {
@@ -132,9 +157,12 @@ class Album extends Component {
              <PlayerBar
                isPlaying={this.state.isPlaying}
                currentSong={this.state.currentSong}
+               currentTime={this.audioElement.currentTime}
+               duration={this.audioElement.duration}
                handleSongClick={() => this.handleSongClick(this.state.currentSong)}
                handlePrevClick={() => this.handlePrevClick()}
                handleNextClick={() => this.handleNextClick()}
+               handleTimeChange={(e) => this.handleTimeChange(e)}
              />
          </section>
          </section>
